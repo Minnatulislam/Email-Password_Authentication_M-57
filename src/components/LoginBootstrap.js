b725b15163a9958bde7d433cea7f363899ac2690
Minnatulislam <minnatulislam86@gmail.com>
2023-03-12 05:39:09 +0600

    login and register validation and verify is complete

--- src/components/LoginBootstrap.js
+++ src/components/LoginBootstrap.js
@@ -1,9 +1,74 @@
-import React from 'react';
+import { getAuth, sendPasswordResetEmail, signInWithEmailAndPassword } from 'firebase/auth';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import app from '../firebase/firebase.init';
+
+const auth = getAuth(app);
 
 const LoginBootstrap = () => {
+    const [success, setSuccess] = useState(false);
+    const [userEmail, setUserEmail] = useState('');
+
+    const handleSubmit = event => {
+        event.preventDefault()
+        setSuccess(false)
+
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+
+        signInWithEmailAndPassword(auth, email, password)
+        .then(result => {
+            const user = result.user
+            console.log(user);
+            setSuccess(true);
+            event.target.reset();
+        })
+        .catch(error => {
+            console.error('error:', error)
+        })
+
+    }
+
+    const handleEmailBlur = event => {
+        const email = event.target.value;
+        setUserEmail(email);
+        console.log(email);
+    }
+
+    const handleForgetPassword = () => {
+        if(!userEmail){
+            alert('Please enter your email address')
+            return;
+        }
+        sendPasswordResetEmail(auth, userEmail)
+        .then( () => {
+            alert('Password Reset email sent. Please check your Email.')
+        })
+        .catch(error => {
+            console.error('error:', error);
+        })
+    }
+
     return (
-        <div>
-            <h2 className='text-success'>Please Login!!!</h2>
+        <div className='w-50 mx-auto '>
+            <h2 className='text-primary'>Please Login Your Account!!!</h2>
+            <form onSubmit={handleSubmit}>
+                <div className="mb-3">
+                    <label htmlFor="formGroupExampleInput" className="form-label">Email</label>
+                    <input onBlur={handleEmailBlur} type="email" name='email' className="form-control" id="formGroupExampleInput" placeholder="Your Email" required/>
+                </div>
+                <div className="mb-3 ">
+                    <label htmlFor="formGroupExampleInput2" className="form-label">Password</label>
+                    <input type="password" name='password' className="form-control " id="formGroupExampleInput2" placeholder="Password" required />
+                </div>
+                <button className="btn btn-primary" type="submit">Login</button>
+
+            </form>
+            {
+                success && <p className='text-success mt-2 fs-5'>Successfully login to the account</p>
+            }
+            <p><small>New to this website? Please <Link to='/register'>Register</Link></small> </p>
+            <p><small>Forget Password? <button type="button" onClick={handleForgetPassword} className="btn btn-link">Reset password</button></small></p>
         </div>
     );
 };
--- src/components/RegisterReactBootstrap.js
+++ src/components/RegisterReactBootstrap.js
@@ -1,7 +1,9 @@
-import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth';
+import { Alert } from 'bootstrap';
+import { createUserWithEmailAndPassword, getAuth, sendEmailVerification, updateProfile } from 'firebase/auth';
 import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { Link } from 'react-router-dom';
 import app from '../firebase/firebase.init';
 
 
@@ -11,45 +13,72 @@ const auth = getAuth(app)
 const RegisterReactBootstrap = () => {
     const [passwordError, setPasswordError] = useState('');
     const [success, setSuccess] = useState(false)
-    const hadleRegister = event =>{
+    const hadleRegister = event => {
         event.preventDefault();
         setSuccess(false)
+        const name = event.target.name.value;
         const email = event.target.email.value;
         const password = event.target.password.value;
-        console.log(email, password);
-        if(!/(?=.*[A-Z].*[A-Z])/.test(password)){
+        // console.log(name, email, password);
+
+        // Validate password
+        if (!/(?=.*[A-Z].*[A-Z])/.test(password)) {
             setPasswordError('Please provide atleast two uppercase')
             return;
         }
-        if(password.length < 6){
+        if (password.length < 6) {
             setPasswordError('Please should be atleast 6 character')
             return;
         }
-        if(!/(?=.*[!@#$&*])/.test(password)){
+        if (!/(?=.*[!@#$&*])/.test(password)) {
             setPasswordError('Please add at least one special character')
             return;
         }
 
 
         setPasswordError('');
-        createUserWithEmailAndPassword(auth, email, password)
-        .then(result => {
-            const user = result.user;
-            console.log(user);
-            setSuccess(true);
-            event.target.reset();
-        })
-        .catch(error => {
-            console.error('error:', error);
-            setPasswordError(error.massage)
-        })
 
+        createUserWithEmailAndPassword(auth, email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+                setSuccess(true);
+                event.target.reset();
+                verifyEmail();
+                updateUserName(name)
+            })
+            .catch(error => {
+                console.error('error:', error);
+                setPasswordError(error.massage)
+            })
+
+    }
+
+    const verifyEmail = () => {
+        sendEmailVerification(auth.currentUser)
+            .then(() => {
+                alert('Please check your email and verify...')
+            })
+    }
+
+    const updateUserName = name => {
+        updateProfile(auth.currentUser, {
+            displayName:name
+        })
+        .then( () =>{
+            console.log('display name updated');
+        })
+        .catch(error => console.error(error))
     }
 
     return (
         <div className='w-50 mx-auto'>
-                <h3 className='text-primary mb-3'>Please Register!!!</h3>
+            <h3 className='text-primary mb-3'>Please Register!!!</h3>
             <Form onSubmit={hadleRegister}>
+                <Form.Group className="mb-3" controlId="formBasicName">
+                    <Form.Label>Your Name</Form.Label>
+                    <Form.Control type="text" name='name' placeholder="Enter your name" />
+                </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
                     <Form.Control type="email" name='email' placeholder="Enter email" />
@@ -73,6 +102,7 @@ const RegisterReactBootstrap = () => {
                     Register
                 </Button>
             </Form>
+            <p><small>Already have an account? Please <Link to='/login'>Login</Link></small> </p>
         </div>
     );
 };
